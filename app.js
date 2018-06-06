c751ab0f8805801fd2442040475682ac31b02300
maryfsc <maryfsc@disroot.org>
2018-06-06 01:53:24 -0300

    Produto final resolvido.

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-var stringCardNumber = prompt('Digite o número do cartão de crédito:');
-
-function isValidCard(stringCardNumber) {
+function isValidCard(){
+  var stringCardNumber = document.getElementById('card-number').value;
+  
   var reversedStringArray = stringCardNumber.split('').reverse();
   
   var numberToValidate = [];
 
-  for (i = 0; i < reversedStringArray.length; i++) {
+  for (i = 0; i < reversedStringArray.length; i++){
     var stringToNumber = parseInt(reversedStringArray[i]);
     numberToValidate.push(stringToNumber);
   }
@@ -14,17 +14,23 @@ function isValidCard(stringCardNumber) {
 
   for (j = 1; j < numberToValidate.length; j = j + 2){
     multiplyingElement = numberToValidate[j] * 2;
-    
     if (multiplyingElement > 9) {
       multiplyingElement -= 9;
     }
 
-    if (multiplyingElement != numberToValidate[j]) {
+    if (multiplyingElement != numberToValidate[j]){
        numberToValidate[j] = multiplyingElement;
-      }      
+    }   
   }  
- 
-  
+  var finalCardNumber = 0;
 
-  return numberToValidate;
+  for (t = 0; t < numberToValidate.length; t++){
+    finalCardNumber += numberToValidate[t]
+  }
+
+  if (finalCardNumber % 10 === 0){
+    return alert('Tudo ok! Seu cartão de crédito é válido!'); 
+  } else {
+    return alert('Seu cartão de crédito não é válido! Tente outro número.');
+  }
 }
