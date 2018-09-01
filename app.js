5a42452dbbb269995f538ecda4585f6898f4b0f3
Mary Francis <maryfsc@disroot.org>
2018-09-01 14:52:40 -0300

    Refatorando

--- app.js
+++ app.js
@@ -1,41 +1,56 @@
-function isValidCard(){
-  var stringCardNumber = document.getElementById('card-number').value;
-
-  if (stringCardNumber === '') {
-    alert('Digite os números do cartão!');
-  } else { 
-    var reversedStringArray = stringCardNumber.split('').reverse();
-
-    var numberToValidate = [];
-
-    for (i = 0; i < reversedStringArray.length; i++){
-      var stringToNumber = parseInt(reversedStringArray[i]);
-      numberToValidate.push(stringToNumber);
-    }
-
-    var multiplyingElement;
-
-    for (j = 1; j < numberToValidate.length; j = j + 2){
-      multiplyingElement = numberToValidate[j] * 2;
-      if (multiplyingElement > 9) {
-        multiplyingElement -= 9;
-      }
-
-      if (multiplyingElement != numberToValidate[j]){
-        numberToValidate[j] = multiplyingElement;
-      }
-    } 
-
-    var finalCardNumber = 0;
-
-    for (t = 0; t < numberToValidate.length; t++){
-      finalCardNumber += numberToValidate[t];
-    }
-
-    if (finalCardNumber % 10 === 0){
-      return alert('Tudo ok! Seu cartão de crédito é válido!'); 
-    } else {
-      return alert('Seu cartão de crédito não é válido! Tente outro número.');
-    }
-  }   
+function cardValidator(input){
+  let arrayNumber = Array.from(input.toString()).map(Number).reverse();
+  return arrayNumber;
 }
+
+function findEvenDigits(digit, index) {
+  if (index % 2 === 0){
+    return digit;
+  } else {
+    if (num * 2 <= 9) {
+      return num * 2;
+    } else {
+      return num * 2 - 9;
+    }
+  }
+}
+
+
+
+  // if (input === '') {
+  //   alert('Digite os números do cartão!');
+  // } else { 
+  //   var reversedStringArray = input.split('').map(Number);
+
+  //   var numberToValidate = [];
+
+  //   for (i = 0; i < reversedStringArray.length; i++){
+  //     var stringToNumber = parseInt(reversedStringArray[i]);
+  //     numberToValidate.push(stringToNumber);
+  //   }
+
+  //   var multiplyingElement;
+
+  //   for (j = 1; j < numberToValidate.length; j = j + 2){
+  //     multiplyingElement = numberToValidate[j] * 2;
+  //     if (multiplyingElement > 9) {
+  //       multiplyingElement -= 9;
+  //     }
+
+  //     if (multiplyingElement != numberToValidate[j]){
+  //       numberToValidate[j] = multiplyingElement;
+  //     }
+  //   } 
+
+  //   var finalCardNumber = 0;
+
+  //   for (t = 0; t < numberToValidate.length; t++){
+  //     finalCardNumber += numberToValidate[t];
+  //   }
+
+  //   if (finalCardNumber % 10 === 0){
+  //     return true; 
+  //   } else {
+  //     return false;
+  //   }
+  // }   
