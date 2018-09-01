0da17617db0dd80807858d0cd377b2d27cfd1416
Mary Francis <maryfsc@disroot.org>
2018-09-01 15:29:10 -0300

    Código refatorado utilizando funcionalidades ES6.

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 function cardValidator(input){
   let reversedNumber = Array.from(input.toString()).map(Number).reverse();
-  let singleDigitNumber = reversedNumber.map(num => findEvenDigits(num, 0));
   
+  let singleDigitNumber = reversedNumber.map(num => multiplyEvenPositions(num, 0));
+  
+  let finalCardNumber = parseInt(singleDigitNumber.toString().split(',').join(''));
+
+
+  return finalCardNumber % 2 ? true : false;
 }
 
-function findEvenDigits(digit, index) {
+function multiplyEvenPositions(digit, index) {
   if (index % 2 === 0){
     return digit;
   } else {
@@ -15,43 +20,3 @@ function findEvenDigits(digit, index) {
     }
   }
 }
-
-
-
-  // if (input === '') {
-  //   alert('Digite os números do cartão!');
-  // } else { 
-  //   var reversedStringArray = input.split('').map(Number);
-
-  //   var numberToValidate = [];
-
-  //   for (i = 0; i < reversedStringArray.length; i++){
-  //     var stringToNumber = parseInt(reversedStringArray[i]);
-  //     numberToValidate.push(stringToNumber);
-  //   }
-
-  //   var multiplyingElement;
-
-  //   for (j = 1; j < numberToValidate.length; j = j + 2){
-  //     multiplyingElement = numberToValidate[j] * 2;
-  //     if (multiplyingElement > 9) {
-  //       multiplyingElement -= 9;
-  //     }
-
-  //     if (multiplyingElement != numberToValidate[j]){
-  //       numberToValidate[j] = multiplyingElement;
-  //     }
-  //   } 
-
-  //   var finalCardNumber = 0;
-
-  //   for (t = 0; t < numberToValidate.length; t++){
-  //     finalCardNumber += numberToValidate[t];
-  //   }
-
-  //   if (finalCardNumber % 10 === 0){
-  //     return true; 
-  //   } else {
-  //     return false;
-  //   }
-  // }   
