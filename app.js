5d002011d1efd55bc84792bd31f198fa05dbe14c
Mary Francis <maryfsc@disroot.org>
2018-09-01 16:53:35 -0300

    Ajustando bugs de retorno de função.

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 function cardValidator(input){
   let reversedNumber = Array.from(input.toString()).map(Number).reverse();
   
-  let singleDigitNumber = reversedNumber.map(num => multiplyEvenPositions(num, 0));
-  
-  let finalCardNumber = parseInt(singleDigitNumber.toString().split(',').join(''));
+  let singleDigitsArray = reversedNumber.map(num => multiplyOddIndexes(num, 0));
 
+  let sum = singleDigitsArray.reduce((a, b) => a + b, 0);
 
-  return finalCardNumber % 2 ? true : false;
+  return sum % 2 === 0 ? true : false;
 }
 
-function multiplyEvenPositions(digit, index) {
+function multiplyOddIndexes(digit, index) {
   if (index % 2 === 0){
     return digit;
   } else {
-    if (num * 2 <= 9) {
-      return num * 2;
+    if (digit * 2 <= 9) {
+      return digit * 2;
     } else {
-      return num * 2 - 9;
+      return digit * 2 - 9;
     }
   }
 }
